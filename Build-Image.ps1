6686f2f8fc683039aecedefdf414485ff1653bec
James Swineson <github@public.swineson.me>
2019-04-22 22:20:10 +0800

    update

--- Build-Image.ps1
+++ Build-Image.ps1
@@ -1,6 +1,3 @@
 Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
-
-netsh winhttp show proxy
-
 Import-Module VMware.PowerCLI
 .\ESXi-Customizer-PS-v2.6.0.ps1 -v67 -vft -load net-igb,net-tulip,net55-r8168 -ipvendor "Jamesits" -outDir $env:BUILD_ARTIFACTSTAGINGDIRECTORY
