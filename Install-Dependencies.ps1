5be8c0aa3211ffdce71f6d3384de57e4555810fc
James Swineson <github@public.swineson.me>
2019-04-22 20:25:12 +0800

    force disable all confirm

--- Install-Dependencies.ps1
+++ Install-Dependencies.ps1
@@ -1,2 +1,5 @@
+# disable confirm
+$ConfirmPreference = None
+
 Install-Module -Name VMware.PowerCLI -Scope CurrentUser -Force -AllowClobber
-Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Force
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false
