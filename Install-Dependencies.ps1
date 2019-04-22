418e19cb22c542b7bffc7dbc7c50f8097d8b460a
James Swineson <github@public.swineson.me>
2019-04-22 20:41:27 +0800

    explicitly import module

--- Install-Dependencies.ps1
+++ Install-Dependencies.ps1
@@ -2,4 +2,5 @@
 $ConfirmPreference="None"
 
 Install-Module -Name VMware.PowerCLI -Scope CurrentUser -Force -AllowClobber
+Install-Module -Name VMware.ImageBuilder -Scope CurrentUser -RequiredVersion 6.7.0.11233116 -Force
 Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false
