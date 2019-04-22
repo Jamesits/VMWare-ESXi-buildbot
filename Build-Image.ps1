58968c4d9ed995964f4020ebb3eb228fa0db850b
James Swineson <github@public.swineson.me>
2019-04-22 20:03:32 +0800

    update filename

--- Build-Image.ps1
+++ Build-Image.ps1
@@ -1,1 +1,1 @@
-.\ESXi-Customizer-PS-v2.6.ps1 -v67 -vft -load net-igb -load net-tulip -load net55-r8168 -ipvendor "Jamesits" -outDir $env:Build.ArtifactStagingDirectory
+.\ESXi-Customizer-PS-v2.6.0.ps1 -v67 -vft -load net-igb -load net-tulip -load net55-r8168 -ipvendor "Jamesits" -outDir $env:Build.ArtifactStagingDirectory
